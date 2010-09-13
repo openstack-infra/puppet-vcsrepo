e572002ab6b9ce15d446ffdab19353d4da18849c
James Turnbull <james@lovedthanlost.net>
2010-09-14 09:00:34 +1000

    Fixed bomb out with an error concerning working_copy_exists? not being defined for svn repos
    Thanks to Bret S. Lambert for the patch

--- lib/puppet/provider/vcsrepo/svn.rb
+++ lib/puppet/provider/vcsrepo/svn.rb
@@ -19,8 +19,12 @@ Puppet::Type.type(:vcsrepo).provide(:svn, :parent => Puppet::Provider::Vcsrepo) 
     end
   end
 
-  def exists?
+  def working_copy_exists?
     File.directory?(File.join(@resource.value(:path), '.svn'))
+  end
+
+  def exists?
+    working_copy_exists?
   end
 
   def destroy
