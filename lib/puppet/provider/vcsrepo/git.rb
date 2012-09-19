de4d60b8b2564842cceb73aa0d8327e326a0e470
Ken Barber <ken@bob.sh>
2012-09-19 20:45:12 +0100

    (#16495, #15660) Fix regression for notifications and pulls on git provider
    The last merge: 4d2942edc26e7cd144a3178a1a7f6470ea401345 brought some
    regression that this patch should hopefully fix.
    
    Firstly, the tool no longer supported updating a branch as the syntax for git
    pull on anything but a branch was invalid.
    
    This also removes the extra call to 'checkout' which was causing behaviour
    to occur without notifying puppet, thus causing subscription notifications
    to not fire.
    
    Conflicts:
    	lib/puppet/provider/vcsrepo/git.rb
    
    Change-Id: Ibc65a088e4be64c4a65a33e5d521ae231152806c

--- lib/puppet/provider/vcsrepo/git.rb
+++ lib/puppet/provider/vcsrepo/git.rb
@@ -98,8 +98,8 @@ Puppet::Type.type(:vcsrepo).provide(:git, :parent => Puppet::Provider::Vcsrepo) 
 
   def update_references
     at_path do
-      git_with_identity('fetch', '--tags', @resource.value(:remote))
       git_with_identity('fetch', @resource.value(:remote))
+      git_with_identity('fetch', '--tags', @resource.value(:remote))
       update_owner_and_excludes
     end
   end
